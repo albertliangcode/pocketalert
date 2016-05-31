a370f697b209dccbb26db1d9acf9e7552986f8b3
Albert Liang <albertliangcode@gmail.com>
2016-05-31 06:04:03 -0700

    Implement POST request
    App can now send a text to a single phone number.

--- application.go
+++ application.go
@@ -4,9 +4,26 @@ import (
     "io/ioutil"
     "log"
     "net/http"
+    "bytes"
+    "encoding/json"
     "os"
     "fmt"
 )
+
+/* NOTE: Taking advantage of other projects?
+ * go-plivo may be a better way to handle in the future
+ */
+
+// TODO: roll id+token and find way to obfuscate
+const auth_id string = "MANMM4ZDRJMMMXZGUZNZ"
+const auth_token string = "YTJlMDg5ZDg0OWIxZTA1OGU4NDNiMzQxOWQyY2Iw"
+const source_phone string = "18056684235"
+
+type plivoMessage struct {
+    Src  string `json:"src,omitempty"`
+    Dst  string `json:"dst,omitempty"`
+    Text string `json:"text,omitempty"`
+}
 
 func main() {
     port := os.Getenv("PORT")
@@ -30,6 +47,8 @@ func main() {
             }
         })
 
+        const plivoBaseUrl string = "https://api.plivo.com/v1/Account/"
+        var plivoApiUrl string = plivoBaseUrl + auth_id + "/Message/"
         http.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request){
             err := r.ParseForm()
             if err != nil {
@@ -37,9 +56,33 @@ func main() {
             }
             numbers := r.PostFormValue("numbers")
             message := r.PostFormValue("message")
+            // TODO: Remove debug println
             fmt.Println(numbers, message)
-            
+           
+            // TODO: use string.Replacer to swap out , with <
+
+            msg := plivoMessage{source_phone, numbers, message}
+            jsonToPlivo, err := json.Marshal(msg)
+
             //Send a post request
+            req, err := http.NewRequest("POST", plivoApiUrl, bytes.NewBuffer(jsonToPlivo))
+            if err != nil {
+                log.Printf("Could not form POST request: ")
+                log.Fatal(err)
+            }
+            req.SetBasicAuth(auth_id, auth_token)
+            req.Header.Add("Content-Type", "application/json")
+            client := &http.Client{}
+            resp, err := client.Do(req)
+            if err != nil {
+                panic(err)
+            }
+            defer resp.Body.Close()
+
+            fmt.Println("response Status:", resp.Status)
+            fmt.Println("response Headers:", resp.Header)
+            body, _ := ioutil.ReadAll(resp.Body)
+            fmt.Println("response Body:", string(body))
 
         })
 
